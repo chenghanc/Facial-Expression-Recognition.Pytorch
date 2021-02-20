34ef1f1763f42eee381ade0735c241d2936d9ff2
chenghanc <chenghan.chung@rwth-aachen.de>
2021-02-20 11:39:01 +0800

    Add files via upload

--- k_fold_train.py
+++ k_fold_train.py
@@ -2,7 +2,7 @@ import os
 
 for i in range(10):
     #cmd = 'python mainpro_CK+.py --model VGG19 --bs 32 --lr 0.01 --fold %d' %(i+1)
-    cmd = 'python mainpro_CK+.py --model VGG19 --bs 512 --lr 0.01 --fold %d' %(i+1)
+    cmd = 'python mainpro_CK+.py --model VGG19 --bs 64 --lr 0.01 --fold %d' %(i+1)
     os.system(cmd)
 print("Train VGG19 ok!")
 
