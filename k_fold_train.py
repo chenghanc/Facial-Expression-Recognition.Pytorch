762c195ed1e38ca6e5f3a2a09a737630aa7f41ef
Cesare <chenghan.chung@rwth-aachen.de>
2021-02-24 10:50:13 +0800

    addadd ResNet50

--- k_fold_train.py
+++ k_fold_train.py
@@ -4,7 +4,8 @@ for i in range(10):
     #cmd = 'python mainpro_CK+.py --model VGG19 --bs 32 --lr 0.01 --fold %d' %(i+1)
     #cmd = 'python mainpro_CK+.py --model VGG19 --bs 64 --lr 0.01 --fold %d' %(i+1)
     #cmd = 'python mainpro_CK+.py --model Resnet18 --bs 64 --lr 0.01 --fold %d' %(i+1)
-    cmd = 'python mainpro_CK+.py --model Resnet34 --bs 64 --lr 0.01 --fold %d' %(i+1)
+    #cmd = 'python mainpro_CK+.py --model Resnet34 --bs 64 --lr 0.01 --fold %d' %(i+1)
+    cmd = 'python mainpro_CK+.py --model Resnet50 --bs 64 --lr 0.01 --fold %d' %(i+1)
     os.system(cmd)
 print("Train VGG19 ok!")
 
