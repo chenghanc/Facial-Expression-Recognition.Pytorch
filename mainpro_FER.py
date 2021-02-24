2bb37a0bda643985785a981777190ae42edb6278
Cesare <chenghan.chung@rwth-aachen.de>
2021-02-24 10:55:28 +0800

    add ResNet50

--- mainpro_FER.py
+++ mainpro_FER.py
@@ -64,10 +64,12 @@ PrivateTestloader = torch.utils.data.DataLoader(PrivateTestset, batch_size=opt.b
 # Model
 if opt.model == 'VGG19':
     net = VGG('VGG19')
-elif opt.model  == 'Resnet18':
+elif opt.model == 'Resnet18':
     net = ResNet18()
 elif opt.model == 'Resnet34':
     net = ResNet34()
+elif opt.model == 'Resnet50':
+    net = ResNet50()
 
 if opt.resume:
     # Load checkpoint.
